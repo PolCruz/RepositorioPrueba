6534c7fece27baa8d7aed082e1b0b345c6c0f6fa
PolCruz.PolCruz <pcruzfisica@gmail.com>
2023-11-28 10:50:57 +0100

    nuevo

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,3 @@
 print("Hola mundo, JUAN JUANITO JUAN JUANITO GOLONDRINA AMIGO DE  LOS PATOS Y DE LAS GALLINAS")
 for i in range(0,50):
-    print("Corregire los eaxmenes a tiempo")
+    print("En serio, corregire los xamenes algun dia")
