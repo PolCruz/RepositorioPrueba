d26c5e3d2f5697102eac3b20a13dfbcb9a0df8e3
PolCruz.PolCruz <pcruzfisica@gmail.com>
2023-11-28 10:49:30 +0100

    ha sido sin querer

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,3 @@
 print("Hola mundo, JUAN JUANITO JUAN JUANITO GOLONDRINA AMIGO DE  LOS PATOS Y DE LAS GALLINAS")
 for i in range(0,5):
-    print("En serio, corregire los examenes algun dia")
+    print("Algun dia corregire los examenes")
